5d904c4a9cda81365f8e13c9dc7f4c522b0754d9
soffiayanchashvili <119891979+soffiayanchashvili@users.noreply.github.com>
2023-05-29 02:33:52 +0400

    did log in/log out pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import person_icon from "./icons/person_icon.png";
+// import password_icon from "./icons/eye_icon.png"
+import login_person_icon from "./icons/login_person_icon.png"
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  const changeLogging = () => {
+    setIsLoggedIn (prev => !prev )
+  }
+
+  const logOut = () => {
+    setUserName ("");
+    changeLogging ();
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn ? (
+        <div className="registration_box">
+          <img src={person_icon} className="human_icon" />
+          <h3>{userName}</h3>
+          <img src={login_person_icon} className="login_person" />
+          <button className="blue_button" onClick={logOut}>LOG OUT</button>
+        </div>
+      ) : (
+        <div className="registration_box">
+          <img src={person_icon} className="human_icon" />
+          <h3>Login</h3>
+          <p>Hello My Friend</p>
+          <div className="input_div">
+            <input type="email" placeholder="Email" onChange={changeLogging}/>
+            <img src={person_icon} className="input_icon" />
+          </div>
+          <div className="input_div">
+            <input
+              type="password"
+              placeholder="Password"
+            />
+            {/* <img src={password_icon} className="input_icon" /> */}
+          </div>
+          <button className="blue_button" onClick={() => setIsLoggedIn(true)}>
+            LOG IN
+          </button>
+        </div>
+      )}
     </div>
   );
 }
